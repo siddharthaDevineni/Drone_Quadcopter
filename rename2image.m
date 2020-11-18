82cf06c704f7d799899b805ebff2a1985a79da5a
siddharthaDevineni <45402807+siddharthaDevineni@users.noreply.github.com>
2020-11-18 02:22:32 +0100

    Update rename2image.m

--- rename2image.m
+++ rename2image.m
@@ -1,12 +1,14 @@
+clear all
+clc
+close all
+%% Renaming any random file names to image1, image2, image3 and so on....source: Matlab Answers
 files = dir('C:\Users\Mahadeva\Desktop\QuadCopter\Drone Images\6thset');
 files = files(3:end);
-% Get all text files in the current folder
-% files = dir('*.txt');
+
 % Loop through each file 
 for id = 1:length(files)
     % Get the file name 
     [~, f,ext] = fileparts(files(id).name);
-%     x = str2num(id);
     rename = sprintf('image%d.png', id) ; 
     movefile(files(id).name, rename); 
 end
