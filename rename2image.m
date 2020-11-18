16662c31bc298f368f142f9682aeafd5e918cecb
siddharthaDevineni <45402807+siddharthaDevineni@users.noreply.github.com>
2020-11-18 12:04:15 +0100

    Update rename2image.m

--- rename2image.m
+++ rename2image.m
@@ -2,7 +2,7 @@ clear all
 clc
 close all
 %% Renaming any random file names to image1, image2, image3 and so on....original source: Matlab Answers
-files = dir('C:\Users\Mahadeva\Desktop\QuadCopter\Drone Images\6thset');
+files = dir('C:\Users\Mahadeva\Desktop\QuadCopter\Drone Images\7thset');
 files = files(3:end);
 
 % Loop through each file 
